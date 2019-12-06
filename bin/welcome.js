a7c47b3ddb40061de0ffa36e75dccd5c9788fdf5
tastafur <ruben.romero@guidesmiths.com>
2019-12-06 11:07:02 +0100

    feature add welcome function

---
+++ bin/welcome.js
@@ -0,0 +1,18 @@
+const welcome = name => {
+  const welcomeMessage = `Hello, ${name}!`;
+  console.log(welcomeMessage);
+
+  const now = new Date().getHours();
+  let prefix = '';
+  if (now > 20 && now < 6) {
+    prefix = 'Buenas noches';
+  } else if (now > 6 && now < 12) {
+    prefix = 'Buenos días';
+  } else {
+    prefix = 'Buenas tardes';
+  }
+
+  console.log(`¡${prefix} ${name}!`);
+};
+
+module.exports = welcome;
