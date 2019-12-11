a2c813c75461a9464e4e3ea455e6ca43fa064b3a
tastafur <ruben.romero@guidesmiths.com>
2019-12-11 22:29:35 +0100

    ft(feature/add/yargs): add option style

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process');
-
-const yargs = require("yargs");
+const yargs = require('yargs');
 const welcome = require('../lib/welcome');
 const userLoop = require('../lib/userLoop');
 
@@ -11,24 +9,13 @@ yargs
     command: '$0 <name>',
     desc: 'ohce is a console application that echoes the reverse of what you input through the console',
     handler: argv => {
-      welcome(argv.name);
-      if (!argv.test)
-        userLoop(argv.name);
-      else
-        exec('npm t', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        console.log(stdout);
-        console.error(stderr);
-      });
+      console.log('argv', argv)
+      welcome(argv.name, argv.colour);
+      userLoop(argv.name, argv.colour);
     }
   })
-  .option('t', {
-    alias: 'test',
-    default: true,
-    defaultDescription: 'Launch the tests',
+  .option('c', {
+    alias: 'colour',
     describe: 'Exec tests'
   })
   .help()
