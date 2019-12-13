9d8599ce97c0ee9c9b7d92bc053bd540ad62d51f
tastafur <ruben.romero@guidesmiths.com>
2019-12-13 20:42:17 +0100

    test(master): test cli

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -2,14 +2,14 @@ const assert = require('assert');
 const { EOL } = require('os');
 const pkg = require('../package')
 
-const cmd = require('../lib/cmd');
+const { execute, ENTER } = require('../lib/cmd');
 
 describe('e2e', () => {
   describe('exec ohce', () => {
 
     it('should be exec ohce return help info and error of its necessary name like parameter', async () => {
       try {
-        await cmd.execute('ohce');
+        await execute('ohce');
       } catch (e) {
         assert.equal('ohce <name>\n\nohce is a console application that echoes the reverse of what you input through\nthe console\n\n _______         __   __                     __\n|       |.-----.|  |_|__|.-----.-----.-----.|__|\n|   -   ||  _  ||   _|  ||  _  |     |__ --| __\n|_______||   __||____|__||_____|__|__|_____||__|\n         |__|\n  --version     Show version number                                    [boolean]\n  -c, --colour  CLI add colours                                        [boolean]\n  --help        Show help                                              [boolean]\n', e)
       }
@@ -18,7 +18,7 @@ describe('e2e', () => {
     it('should be exec ohce --version and return help version', async () => {
       let response;
       try {
-        response = await cmd.execute('ohce', ['pepe', '--version']);
+        response = await execute('ohce', ['pepe', '--version']);
       } catch (e) {
         console.log('error',e)
       }
@@ -28,11 +28,54 @@ describe('e2e', () => {
     it('should be exec ohce --help and return help info', async () => {
       let response;
       try {
-        response = await cmd.execute('ohce', ['pepe', '--help']);
+        response = await execute('ohce', ['pepe', '--help']);
       } catch (e) {
         console.log('error',e)
       }
       assert.equal('ohce <name>\n\nohce is a console application that echoes the reverse of what you input through\nthe console\n\n _______         __   __                     __\n|       |.-----.|  |_|__|.-----.-----.-----.|__|\n|   -   ||  _  ||   _|  ||  _  |     |__ --| __\n|_______||   __||____|__||_____|__|__|_____||__|\n         |__|\n  --version     Show version number                                    [boolean]\n  -c, --colour  CLI add colours                                        [boolean]\n  --help        Show help                                              [boolean]\n', response)
+
+    });
+
+    it('should be exec ohce, input a palindromo and return this message ¡Bonita palabra!', async () => {
+      let response, objetiveLine;
+      try {
+        response = await execute('ohce', ['pepe'], [
+          'Arriba birra',
+          ENTER,
+          'Stop!',
+          ENTER
+        ]);
+        objetiveLine = response
+          .trim()
+          .split(EOL)
+
+      } catch (e) {
+        console.log('error',e)
+      }
+      assert.equal('¡Bonita palabra!', objetiveLine[objetiveLine.length - 2])
+
+    });
+
+    it('should be exec ohce and stop ohce', async () => {
+      let response, lastLine;
+      try {
+        response = await execute('ohce', ['pepe'], [
+          'Arriba birra',
+          ENTER,
+          'Stop!',
+          ENTER
+        ]);
+        lastLine = response
+          .trim()
+          .split(EOL)
+          .pop()
+
+        console.log('response', response)
+        console.log('lastLine', lastLine)
+      } catch (e) {
+        console.log('error',e)
+      }
+      assert.equal('ohce > Adios pepe', lastLine)
 
     });
 
@@ -42,7 +85,7 @@ describe('e2e', () => {
 
     it('should be exec ohce-power return help info and error of its necessary name like parameter', async () => {
       try {
-        await cmd.execute('ohce-power');
+        await execute('ohce-power');
       } catch (e) {
         assert.equal('ohce-power <name>\n\nohce is a console application that echoes the reverse of what you input through\nthe console\n\n _______         __   __                     __\n|       |.-----.|  |_|__|.-----.-----.-----.|__|\n|   -   ||  _  ||   _|  ||  _  |     |__ --| __\n|_______||   __||____|__||_____|__|__|_____||__|\n         |__|\n  --version     Show version number                                    [boolean]\n  -c, --colour  CLI add colours                                        [boolean]\n  --help        Show help                                              [boolean]\n', e)
       }
@@ -51,7 +94,7 @@ describe('e2e', () => {
     it('should be exec ohce-power --version and return help version', async () => {
       let response;
       try {
-        response = await cmd.execute('ohce-power', ['pepe', '--version']);
+        response = await execute('ohce-power', ['pepe', '--version']);
       } catch (e) {
         console.log('error',e)
       }
@@ -61,7 +104,7 @@ describe('e2e', () => {
     it('should be exec ohce-power --help and return help info', async () => {
       let response;
       try {
-        response = await cmd.execute('ohce-power', ['pepe', '--help']);
+        response = await execute('ohce-power', ['pepe', '--help']);
       } catch (e) {
         console.log('error',e)
       }
